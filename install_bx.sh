8eff7901f2b6b126735d68c5d706b05bc12d64d9
ROCIEL C. BUICO <buicor@ph.ibm.com>
2017-06-12 15:13:37 +0800

    added ls

--- install_bx.sh
+++ install_bx.sh
@@ -17,7 +17,8 @@ bx plugin install container-service -r Bluemix
 chmod +x /tmp/Bluemix_CLI/bin/*
 
 echo "Install kubectl"
-wget --quiet --output-document=/tmp/Bluemix_CLI/bin/  https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+wget --quiet --output-document=/tmp/Bluemix_CLI/bin/kubectl  https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+ls -ltr /tmp/Bluemix_CLI/bin/
 chmod +x /tmp/Bluemix_CLI/bin/kubectl
 
 if [ -n "$DEBUG" ]; then
